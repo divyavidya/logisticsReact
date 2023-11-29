481442f1b2a152abcffc6fcecd0d5c4c509f28bf
Karishma Shaik <shaikkarishma4689@gmail.com>
2023-11-29 19:40:26 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 //import LoginDemo from './features/auth/login';
 import CustomerDashboard from './features/customer/dasboard';
+import LoginDemo from './features/auth/login';
+import SignUp from './features/auth/signup';
+import Logout from './features/auth/logout';
 
 function App() {
   return (
     <div className="App">
-      <CustomerDashboard/>
+      <Routes>
+        <Route path="/" element={<CustomerDashboard/>}></Route>
+        <Route path="/customer/dashboard" element={<CustomerDashboard/>}></Route>
+        <Route path="/auth/login" element={<LoginDemo/>}></Route>
+          <Route path="/auth/signup" element={<SignUp/>}></Route>
+          <Route path="/auth/logout" element={<Logout/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/features/auth/logout.js
@@ -0,0 +1,6 @@
+function Logout(){
+    return(<div>
+<h1>Logout</h1>
+    </div>)
+}
+export default Logout;
